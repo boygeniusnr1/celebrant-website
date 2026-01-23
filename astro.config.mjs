01fb5ccc81bbb64b305a5583618422523d189486
boygeniusnr1 <boygeniusnr1@gmail.com>
2026-01-23 15:08:10 +1300

    (Hopefully) fix build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://celebrant-website.vercel.app",
   output: "server",
+  adapter: vercel(),
   integrations: [
     sitemap({
       filter: (page) => page !== "https://celebrant-website.vercel.app/success",
